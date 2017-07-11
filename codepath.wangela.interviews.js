4dda73dd90ee300621798c28236eb3af39d47eaf
wangela <wangela@users.noreply.github.com>
2017-07-11 00:20:23 -0700

    Prework up to Checkpoint 3 without complexity optimization

--- codepath.wangela.interviews.js
+++ codepath.wangela.interviews.js
@@ -70,32 +70,43 @@ function prettyPrint(A) {
     var sqArray = [];
     var dir = 0;
     var top = 0;
-    var right = dim;
-    var bottom = dim;
+    var right = dim - 1;
+    var bottom = dim - 1;
     var left = 0;
 
-    while (A > 0 && bottom > top) {
+    for (x = 0; x < dim; x++) {
+        sqArray[x] = [];
+    }
+
+    while (A > 0 && bottom >= top) {
         if (dir == 0) {
-            for (i = left; i < right; i++) {
+            for (i = left; i <= right; i++) {
                 sqArray[top][i] = A;
+                console.log('sqArray[' + top + "][" + i + "] = " + A);
             }
             top++;
             dir = 1;
-        } else if (dir == 1) {
-            for (j = top; j < bottom; j++) {
+        }
+        if (dir == 1) {
+            for (j = top; j <= bottom; j++) {
                 sqArray[j][right] = A;
+                console.log('sqArray[' + j + "][" + right + "] = " + A);
             }
             right--;
             dir = 2;
-        } else if (dir == 2) {
-            for (i = right - 1; i >= left; i--) {
-                sqArray[i][bottom] = A;
+        }
+        if (dir == 2) {
+            for (i = right; i >= left; i--) {
+                sqArray[bottom][i] = A;
+                console.log('sqArray[' + i + "][" + bottom + "] = " + A);
             }
             bottom--;
             dir = 3;
-        } else if (dir == 3) {
-            for (j = bottom - 1; j >= top; j--) {
+        }
+        if (dir == 3) {
+            for (j = bottom; j >= top; j--) {
                 sqArray[j][left] = A;
+                console.log('sqArray[' + j + "][" + left + "] = " + A);
             }
             left++;
             dir = 0;
@@ -106,4 +117,184 @@ function prettyPrint(A) {
 
     console.log(JSON.stringify(sqArray));
     return sqArray;
+}
+
+function findCount(A, B) {
+    var lowIndex = 0;
+    var highIndex = 0;
+
+    function oneWay(C, D, highFirst) {
+        var low = 0;
+        var high = C.length - 1;
+        var result = -1;
+
+        while (low <= high) {
+            var mid = Math.floor(low + (high - low)/2);
+            if (C[mid] == D) {
+                result = mid;
+                if (highFirst) {
+                    low = mid + 1;
+                }
+                else high = mid - 1;
+            } else if (D < C[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    highIndex = oneWay(A, B, true);
+
+    if (highIndex == -1) {
+        return 0;
+    } else {
+        lowIndex = oneWay(A, B, false);
+    }
+
+    return highIndex - lowIndex + 1;
+}
+
+function findMin (A) {
+    var n = A.length;
+    var low = 0;
+    var high = n - 1;
+
+    while (low <= high) {
+        if (A[low] <= A[high]) return low; // Case 1
+
+        var mid = Math.floor((high + low)/2);
+        console.log("mid = " + mid);
+        var next = (mid + 1) % n;
+        var prev = (mid + n - 1) % n;
+
+        if ((A[mid] <= A[next]) && (A[mid] <= A[prev])) { // Case 2
+            console.log("case 2");
+            return mid;
+        } else if (A[mid] <= A[high]) { // Case 3
+            console.log("case 3");
+            high = mid - 1;
+        } else if (A[mid] >= A[low]) { // Case 4
+            console.log("case 4")
+            low = mid + 1;
+        }
+    }
+
+    console.log("oops2");
+}
+
+function MinHeap() {
+    this.content = [];
+}
+
+MinHeap.prototype = {
+    push: function(element) {
+        this.content.push(element);
+    },
+
+    pop: function() {
+        var result = this.content[0];
+        var end = this.content.pop();
+
+        if (this.content.length > 0) {
+            this.content[0] = end;
+            this.sinkDown(0);
+        }
+
+        return result;
+    },
+
+    remove: function(node) {
+        var length = this.content.length;
+
+        for (i = 0; i < length; i++) {
+            if (this.content[i] != node) continue;
+
+            var end = this.content.pop();
+
+            if (i == length - 1) break;
+
+            this.content[i] = end;
+            this.bubbleUp(i);
+            this.sinkDown(i);
+            break;
+        }
+    },
+
+    size: function() {
+        return this.content.length;
+    },
+
+    bubbleUp: function(n) {
+        var element = this.content[n];
+
+        while (n > 0) {
+            var parentN = Math.floor((n + 1)/2) - 1
+            var parent = this.content[parentN];
+
+            if (element < parent) {
+                this.content[parentN] = element;
+                this.content[n] = parent;
+                n = parentN;
+            }
+        }
+    },
+
+    sinkDown: function(n) {
+        var length = this.content.length;
+        var element = this.content[n];
+
+        while (true) {
+            var child2N = (n + 1) * 2;
+            var child1N = child2N - 1;
+
+            var swap = null;
+
+            if (child1N < length) {
+                var child1 = this.content[child1N];
+
+                if (child1 > element) {
+                    swap = child1N;
+                }
+            }
+
+            if (child2N < length) {
+                var child2 = this.content[child2N];
+
+                if (child2 > (swap == null ? element : child1)) {
+                    swap = child2N;
+                }
+            }
+
+            if (swap == null) break;
+
+            this.content[n] = this.content[swap];
+            this.content[swap] = element;
+            n = swap;
+        }
+    }
+}
+
+function kthSmallest(A, k) {
+    var heap = new MinHeap(function(x) {return x});
+    var result = -1;
+
+    for (i = 0; i < k; i++) {
+        heap.push(A[i]);
+        heap.bubbleUp(heap.size - 1);
+    }
+
+    for (i = k; i < A.length; i++) {
+        heap.push(A[i]);
+        heap.bubbleUp(heap.size - 1);
+        heap.remove(k);
+    }
+
+    for (i = 0; i < k; i++) {
+        result = heap.pop();
+    }
+
+    return result;
 }
--- codepath.wangela.interviews.py
+++ codepath.wangela.interviews.py
@@ -7,3 +7,97 @@ def rotateArray(a, b):
         for j in xrange(shift):
         	ret.append(a[j])
         return ret
+
+def oneWaySearch(A, B, highFirst):
+	low = 0
+	high = len(A) - 1
+	result = -1
+
+	while (low <= high):
+		mid = (high + low) / 2
+		if (A[mid] == B):
+			result = mid
+			if highFirst:
+				low = mid + 1
+			else:
+				high = mid - 1
+		elif (B < A[mid]):
+			high = mid - 1
+		else:
+			low = mid + 1
+	return result
+
+def searchRange(A, B):
+	highIndex = oneWaySearch(A, B, True)
+	if (highIndex == -1):
+		return 0
+	else:
+		lowIndex = oneWaySearch(A, B, False)
+
+	return (lowIndex, highIndex)
+
+import heapq
+
+def kthSmallest(A, k):
+	myHeap = []
+	result = -1
+
+	for h in range(k):
+		heapq.heappush(myHeap,A[h])
+	for i in range(k, len(A)):
+		heapq.heappush(myHeap,A[i])
+		myHeap = myHeap[:k]
+	for j in range(k):
+		result = heapq.heappop(myHeap)
+	return result
+
+def naiveShallowRange(A, B, C):
+    pivotCount = 0
+    pSum = A[0]
+    tempArr = []
+
+    if (pSum <= C):
+        if (pSum >= B):
+            pivotCount += 1
+        if (len(A) > 1):
+        	tempArr = A[1:]
+        	tempArr[0] += A[0]
+        	pivotCount += naiveShallowRange(tempArr, B, C)
+
+    return pivotCount
+
+
+def naiveNumRange(A, B, C):
+	pivotCount = 0
+	pSum = A[0]
+	tempArr = []
+
+	while (len(A) > 1):
+		if (pSum <= C):
+			if (pSum >= B):
+				pivotCount += 1
+			tempArr = A[1:]
+			tempArr[0] += A[0]
+			pivotCount += naiveShallowRange(tempArr, B, C)
+		A = A[1:]
+		pSum = A[0]
+
+	if (A[0] >= B and A[0] <= C):
+		pivotCount += 1
+
+	return pivotCount
+
+def numRange(A, B, C):
+	sumCount = 0
+
+	for index, val in enumerate(A):
+		if (val > C): continue
+		if (val >= B):
+			sumCount += 1
+		currSum = val
+		for j in A[(index + 1):]:
+			currSum += j
+			if (currSum > C): break
+			if (currSum >= B):
+				sumCount += 1
+	return sumCount
