df4d1e5b95b0af3551a919daa4cdd31adaeb75aa
wangela <wangela@users.noreply.github.com>
2017-11-23 22:29:33 -0800

    Complete checkpoint 5 question 1

--- prework/checkpoint5.py
+++ prework/checkpoint5.py
@@ -1,3 +1,68 @@
+def permute(A):
+    # Argument:
+    #   A is a list of integers that might contain duplicates
+    # Output:
+    #   Return a list of all possible unique permutations of the list A
+    #   This will be a list of lists
+    #   Cannot use library functions for generating permutations
+
+    permutations = []
+    hPermutations = set()
+    tPermutations = []
+    lPermutations = []
+    B = []
+    if len(A) == 1:
+        B = list(A)
+        return [B]
+    else:
+        last = A.pop()
+        rPermutations = permute(A)
+        for each in rPermutations:
+            i = 0
+            while i <= len(each):
+                pList = each.copy()
+                pList.insert(i, last)
+                permutations.append(pList)
+                i += 1
+
+    for item in permutations:
+        tupleItem = tuple(item)
+        hPermutations.add(tupleItem)
+    tPermutations = list(hPermutations)
+    for t in tPermutations:
+        listItem = list(t)
+        lPermutations.append(listItem)
+
+    return lPermutations
+
+
+def permuteBasic(A):
+    # Not time efficient
+
+    permutations = []
+    B = []
+    if len(A) == 1:
+        B = list(A)
+        return [B]
+    else:
+        last = A.pop()
+        rPermutations = permute(A)
+        for each in rPermutations:
+            i = 0
+            while i <= len(each):
+                pList = each.copy()
+                pList.insert(i, last)
+                sPermutations = set()
+                for item in permutations:
+                    tupleItem = tuple(item)
+                    sPermutations.add(tupleItem)
+                tuplePlist = tuple(pList)
+                if tuplePlist not in sPermutations:
+                    permutations.append(pList)
+                i += 1
+    return permutations
+
+
 def longestConsecutive(A):
     # Argument:
     #   A is a list of unsorted integers
