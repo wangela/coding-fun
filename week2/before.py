7a630ec6a9f4ef84291116b939e5d49618cf4374
wangela <wangela@users.noreply.github.com>
2017-12-04 23:49:17 -0800

    Complete week 2 "before" exercises for Linked Lists and add gif

--- week2/before.py
+++ week2/before.py
@@ -95,4 +95,146 @@ def twoSum(A, B):
 
 
 # LINKED LISTS
-## 
+## List 2 pointer
+### Palindrome List
+# Definition for singly-linked list.
+class ListNode:
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
+def convertToLinkedList(A):
+    x = ListNode(A[0])
+    head = x
+
+    for each in A[1:]:
+        y = ListNode(each)
+        x.next = y
+        x = y
+
+    return head
+
+def lPalin(A):
+    # Parameters:
+    #   A is the head node of a singly linked list
+    # Output:
+    #   Return 1 if it is a palindrome
+    #   Return 0 if it is not a palindrome
+    # Goal:
+    #   Linear O(n) time and Constant O(1) space
+    # Example:
+    #   List 1 --> 2 --> 1 --> null is a palindrome. Return 1
+    #   List 1 --> 2 --> 3 --> null is not a palindrome. Return 0
+    # Inefficient Approach:
+    #   As we walk through; make an array copy
+    #   Make pointers to the beginning and ends of the array and walk toward the middle
+    #   If elements at pointers match until the middle, return 1
+    #   If any elements do not match on the way, return 0
+    #   O(n) time but also O(n) space
+    # Efficient Approach:
+    #   As we walk through; make a prev property to convert it to a doubly linked list
+    #   Walk through again from both ends and compare
+    #   O(n) time and O(1) space
+    first = ListNode(A.val)
+    this = first
+    current = A
+    prev = None
+
+    while current.next != None:
+        second = ListNode(current.next.val)
+        next = current.next
+        first.next = second
+        current.next = prev
+        prev = current
+        current = next
+        first = second
+    current.next = prev
+    reverse = current
+    forward = this
+
+    while forward != None and reverse != None:
+        if forward.val != reverse.val:
+            return 0
+        else:
+            forward = forward.next
+            reverse = reverse.next
+
+    return 1
+
+
+## List Math
+### Add Two Numbers As Lists
+class ListNode:
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
+def addTwoNumbers(A, B):
+    currA = A
+    currB = B
+    carry = [0]
+    answer = None
+    sumList = None
+
+    while currA != None or currB != None:
+        currC = 0
+        if int(carry[0]) > 0:
+            currC = int(carry[-1])
+            carry = carry[:-1]
+            if len(carry) == 0:
+                carry = [0]
+        if currA == None:
+            sum = currB.val + currC
+        elif currB == None:
+            sum = currA.val + currC
+        else:
+            sum = currA.val + currB.val + currC
+        if sum > 9:
+            sumStr = str(sum)
+            sumLetters = list(sumStr)
+            if sumList != None:
+                next = ListNode(int(sumLetters[-1]))
+                sumList.next = next
+                sumList = next
+            else:
+                answer = ListNode(int(sumLetters[-1]))
+                sumList = answer
+            carry = sumLetters[:-1]
+        else:
+            if sumList != None:
+                next = ListNode(sum)
+                sumList.next = next
+                sumList = next
+            else:
+                answer = ListNode(sum)
+                sumList = answer
+        if currA == None:
+            currB = currB.next
+        elif currB == None:
+            currA = currA.next
+        else:
+            currA = currA.next
+            currB = currB.next
+
+    if int(carry[0]) > 0:
+        for digit in carry:
+            sumList.next = ListNode(int(digit))
+            sumList = sumList.next
+    sumList.next = None
+
+    return answer
+    # Parameters:
+    #   A is the head node of a linked list representing a non-negative number
+    #   B is the head node of a linked list representing a non-negative number
+    #   Digits are stored in reverse order (smallest to largest magnitude),
+    #   each node containing a single digit
+    # Output:
+    #   Return the head node in the linked list representing the sum of the two numbers
+    #   Ensure there are no trailing zeros in the result
+    # Example:
+    #   (2 -> 4 -> 3) + (5 -> 6 -> 4) = 342 + 564 = 807 = (7 -> 0 -> 8)
+    # Test Cases:
+    #   A or B is 0
+    #   A = 0 and B = 0
+    #   Cause a trailing zero and remove it
+    #   Can input have trailing zeros?
